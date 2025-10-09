9315357b0037b29a4fdf0e69f64b8bd6d40f19e3
tjeddawi <talaljeddawi@gmail.com>
2025-10-09 12:49:42 -0700

    Added all final snacks

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,7 +1,12 @@
+import { printFeatureMessage } from "./animation";
+
 export function printSnacks() {
-    const snacks: string[] = ['Cookies', 'Candy', 'Chocolate', 'Fruit'];
+    const snacks: string[] = ['Cookies', 'Candy', 'Chocolate', 'Fruit', 'Popcorn', 'Carrots'];
     console.log(snacks.join(', '));
 }
 
 // Call the function
 printSnacks();
+
+printFeatureMessage('Snacks', 'italic');
+printFeatureMessage('Snacks', 'bold');
