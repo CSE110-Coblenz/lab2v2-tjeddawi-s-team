250b284d0e502636d48397f1ef11c12206430b83
tjeddawi <talaljeddawi@gmail.com>
2025-10-09 12:10:19 -0700

    Added animation.ts file containing a function which bolds or italicizes inputted features.

---
+++ src/animation.ts
@@ -0,0 +1,8 @@
+function printFeatureMessage(featureName: string, style: 'bold' | 'italic' = 'bold'): void {
+    let message = `${featureName}`;
+    if (style === 'bold') {
+        console.log('\x1b[1m%s\x1b[0m', `${message}`);
+    } else {
+        console.log('\x1b[3m%s\x1b[0m', `${message}`);
+    }
+}
